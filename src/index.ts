3cb77fabe12bf43e178f35aaa06db9ea59914ff2
Lukas Hechenberger <admin@ls-age.com>
2019-03-11 12:30:02 +0100

    style: Setup tslint

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { join, resolve } from 'path';
 import MagicString, { SourceMap } from 'magic-string';
-import { createFilter } from 'rollup-pluginutils';
+import { join, resolve } from 'path';
 import rollup from 'rollup';
+import { createFilter } from 'rollup-pluginutils';
 
 interface Options {
-  include?: string | string[],
-  exclude?: string | string[],
-  shebang?: string,
+  include?: string | string[];
+  exclude?: string | string[];
+  shebang?: string;
 }
 
 const plugin: rollup.PluginImpl<Options> = ({
   include = ['**/cli.js', '**/bin.js'],
   exclude,
   shebang = '#!/usr/bin/env node',
-} : Options = {}) => {
+}: Options = {}) => {
   const filter = createFilter(include, exclude) as (fileName: string) => boolean;
 
   return { name: 'shebang',
@@ -33,7 +33,8 @@ const plugin: rollup.PluginImpl<Options> = ({
       return {
         code: s.toString(),
         // Need to cast version, as it's declared string
-        // See: https://github.com/Rich-Harris/magic-string/pull/155 (already merged, will be in next release)
+        // See: https://github.com/Rich-Harris/magic-string/pull/155
+        // (already merged, will be in next release)
         map: s.generateMap({ hires: true }) as SourceMap & { version: number },
       };
     },
