dbe556933b2386c4974a9cfee0688e59b1d354c7
ls-age CI <ci@ls-age.com>
2019-03-11 11:45:56 +0000

    chore(release): Add 0.1.0 release files [ci skip]

---
+++ out/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var MagicString = _interopDefault(require('magic-string'));
+var path = require('path');
+var rollupPluginutils = require('rollup-pluginutils');
+
+const plugin = ({
+  include = ['**/cli.js', '**/bin.js'],
+  exclude,
+  shebang = '#!/usr/bin/env node'
+} = {}) => {
+  const filter = rollupPluginutils.createFilter(include, exclude);
+  return {
+    name: 'shebang',
+
+    renderChunk(code, {
+      fileName
+    }, {
+      dir,
+      file
+    }) {
+      const outPath = dir ? path.join(dir, fileName) : file || fileName;
+
+      if (!filter(path.resolve(outPath))) {
+        return null;
+      }
+
+      const s = new MagicString(code);
+      s.prepend(`${shebang}
+
+`);
+      return {
+        code: s.toString(),
+        // Need to cast version, as it's declared string
+        // See: https://github.com/Rich-Harris/magic-string/pull/155
+        // (already merged, will be in next release)
+        map: s.generateMap({
+          hires: true
+        })
+      };
+    }
+
+  };
+};
+
+module.exports = plugin;
+//# sourceMappingURL=index.js.map
