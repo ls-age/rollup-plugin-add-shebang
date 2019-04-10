4d9ccf19187ea346499d7444d19f4e0f3fcb748f
Manuel Fernandez <manferlo81@gmail.com>
2019-04-10 14:02:43 +0700

    added test for new feature

--- test/_helpers.ts
+++ test/_helpers.ts
@@ -1,12 +1,13 @@
 import * as rollup from 'rollup';
 import shebang from '../src/index';
 
-export function bundleSingle() {
+export function bundleSingle(options?: object) {
   return rollup.rollup({
     input: ['./test/fixtures/single.js'],
     plugins: [
       shebang({
         include: 'single.js',
+        ...options,
       }),
     ],
   });
