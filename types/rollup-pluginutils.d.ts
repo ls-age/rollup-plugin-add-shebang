4ad5ab4d0d05563405126f80803a519ca4ab616e
LukasHechenberger <admin@ls-age.com>
2019-04-12 13:39:46 +0200

    chore(package): Update rollup-pluginutils

--- types/rollup-pluginutils.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'rollup-pluginutils' {
-  export function createFilter(
-    include?: Array<string | RegExp> | string | RegExp | null,
-    exclude?: Array<string | RegExp> | string | RegExp | null
-  ): (id: string | any) => boolean;
-}
